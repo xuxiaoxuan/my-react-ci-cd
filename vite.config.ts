a0659bbb208fff468f5cd9172fdd857bf055e7fc
xuxiaoxuan <603824778@qq.com>
2026-03-19 10:39:43 +0800

    fix：修复网站白屏问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-react-ci-cd/', // 这里的名字必须和你的 GitHub 仓库名一模一样
   plugins: [react()],
 })
